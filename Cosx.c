e9ba0042425325af98d2a49fe05f5ac459ddbad8
Yadhu Krishna <62097855+yadhukrishna77@users.noreply.github.com>
2020-03-13 21:04:11 +0530

    Update Cosx.c

--- Cosx.c
+++ Cosx.c
@@ -1,13 +1,13 @@
-/*The cos(x) series is cos(x)=1-(x^2/2!)+(x^4/4!)-(x^6/6!).......*/
+
 #include<stdio.h>
 #include<math.h>
 void main()
 {
 	int n,x1,i,j;
 	float x,s=-1,cosx=1,fact;
-	printf("Enter the number of terms in series(degree of precision)\n");//That is the precision in the value of cos(x),as number of terms increses precesion increases
+	printf("Enter the number of terms in series\);
 	scanf("%d",&n);
-	printf("enter the angle in degrees\n");
+	printf("enter the angle in degree\n");
 	scanf("%f",&x);
 	x1=x;
 	x=x*(3.142/180);
